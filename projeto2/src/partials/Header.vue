f941dc35063ab07eea3657d25992ab37c4173569
Augusto Soares <gutosoares@outlook.com>
2017-10-24 00:18:59 -0200

    Create links on header component.

--- projeto2/src/partials/Header.vue
+++ projeto2/src/partials/Header.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="header">
-    <nav class="navbar navbar-expand-lg bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="#">EventVue</a>
+        <router-link class="navbar-brand" to="/">EventVue</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <router-link class="nav-item nav-link active" to="/">Home</router-link>
+            <router-link class="nav-item nav-link active" to="/events">Home</router-link>
             <!-- <router-link class="nav-item nav-link" to="/add">Add Evento</router-links> -->
           </div>
         </div>
