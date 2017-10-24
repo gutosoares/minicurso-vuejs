9f27750ef27d1f708fef340277e9b51426da2224
Augusto Soares <gutosoares@outlook.com>
2017-10-23 23:17:26 -0200

    Create header component.

---
+++ projeto2/src/partials/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="header">
+    <nav class="navbar navbar-expand-lg bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">EventVue</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <router-link class="nav-item nav-link active" to="/">Home</router-link>
+            <!-- <router-link class="nav-item nav-link" to="/add">Add Evento</router-links> -->
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
+
+
