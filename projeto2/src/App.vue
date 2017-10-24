85e56399cb52406db4ded668c2892f28fc0fce6a
Augusto Soares <gutosoares@outlook.com>
2017-10-23 23:17:55 -0200

    Import header component on root app component.

--- projeto2/src/App.vue
+++ projeto2/src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div id="app">
-    <main>
+    <app-header/>
+    <main class="container">
       <router-view></router-view>
     </main>
   </div>
 </template>
 
 <script>
+import Header from './partials/Header'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    'app-header': Header
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
