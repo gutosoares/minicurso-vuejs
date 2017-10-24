f5f16699465bb755462bcdbdd6fe4c68e0bfe16a
Augusto Soares <gutosoares@outlook.com>
2017-10-23 22:33:06 -0200

    Create not found component.

---
+++ projeto2/src/components/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="not-found">
+    <h1>404</h1>
+    <h3>Página não encontrada!</h3>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+#not-found {
+   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
