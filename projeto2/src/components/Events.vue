17cfc94e66c27e50f70bc72d2e1142f4b91133db
Augusto Soares <gutosoares@outlook.com>
2017-10-23 22:30:18 -0200

    Create events component.

---
+++ projeto2/src/components/Events.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="events">
+    <h1>Eventos Cadastrados</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Titulo</th>
+          <th>Palestrante</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>teste</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'events',
+
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
