af7d2998431c76953e0e38b8fe90de4dbe67fd73
Augusto Soares <gutosoares@outlook.com>
2017-10-24 01:38:35 -0200

    Create add route.

--- projeto2/src/router/index.js
+++ projeto2/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Events from '@/components/Events'
+import Details from '@/components/Details'
+import Add from '@/components/Add'
 import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
@@ -15,6 +17,14 @@ export default new Router({
     {
       path: '/events',
       component: Events
+    },
+    {
+      path: '/details/:id',
+      component: Details
+    },
+    {
+      path: '/add',
+      component: Add
     },
     {
       path: '*',
