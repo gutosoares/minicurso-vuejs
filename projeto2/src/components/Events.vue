c882d9887e2a7fbf01eee74251fdfdcf72ace61d
Augusto Soares <gutosoares@outlook.com>
2017-10-24 00:19:14 -0200

    List all events.

--- projeto2/src/components/Events.vue
+++ projeto2/src/components/Events.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="events">
-    <h1>Eventos Cadastrados</h1>
-    <table>
+    <div class="jumbotron">
+      <h1 class="display-3">Eventos Cadastrados</h1>
+    </div>
+    <table class="table">
       <thead>
         <tr>
           <th>Titulo</th>
           <th>Palestrante</th>
           <th>Categoria</th>
           <th>Data</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>teste</td>
+        <tr v-for="event in events" :key="event.id">
+          <td>{{ event.title }}</td>
+          <td>{{ event.speaker }}</td>
+          <td>{{ event.category }}</td>
+          <td>{{ event.date }}</td>
+          <td>
+            <router-link class="btn btn-primary" :to="'/details/' + event.id">Detalhes</router-link>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -25,8 +34,24 @@ export default {
 
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      events: []
     }
+  },
+
+  methods: {
+    fetchEvents() {
+      this.$http.get('http://localhost:3000/events')
+        .then(response => {
+          this.events = response.body
+        })
+        .catch(error => {
+          console.log('Error: ', error)
+        })
+    }
+  },
+
+  created() {
+    this.fetchEvents()
   }
 }
 </script>
